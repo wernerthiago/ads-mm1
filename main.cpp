fc86f9255c7d8d0d1bb58d34efa764b9ca2818ee
werner <wernerthiago@gmail.com>
2017-08-30 13:56:01 -0300

    Com problemas em uma das metricas.

--- main.cpp
+++ main.cpp
@@ -10,38 +10,16 @@ using namespace std;
 
 #define HUGE_VAL 1000000000000000000000000000000000000000000000000000000.0
 #define T_MAX 100.0
+static int ID = 0;
 
 enum Server {
 	IDLE, BUSY
 };
 
-class Statistics {
-private:
-	double processingTime = 0.0;
-	double simulationTime = 0.0;
-	double totalTime = processingTime + simulationTime;
-	MetodoCongruente crand = MetodoCongruente(1103515245,12345,2147483648,rand());
-
-public:
-	Statistics() {
-
-	}
-
-	void update() {
-
-	}
-
-	double rand_exp(double lambda){
-		double y = - 1 + ((double) this->crand.GeraNumeroAleatorio() / (RAND_MAX)) + 1;
-		double x;
-
-		return x = -lambda*log(y);
-	}
-};
-
 class Task {
 public:
 	double sim_time = 0.0;
+	int ID;
 
 	Task() {
 
@@ -49,6 +27,66 @@ public:
 
 	Task(double sim_time) {
 		this->sim_time = sim_time;
+	}
+};
+
+class Statistics {
+private:
+	double arrivalTime = 0.0;
+	double departureTime = 0.0;
+	double requisitionInterval[100];
+	int aux = 0;
+	MetodoCongruente crand = MetodoCongruente(1103515245,12345,2147483648,rand());
+
+public:
+	Statistics() {
+
+	}
+
+	void setarrivalTime(double value){
+		this->arrivalTime = value;
+	}
+
+	void setdepartureTime(double value){
+		this->departureTime = value;
+	}
+
+	void update() {
+		requisitionInterval[aux] = departureTime - arrivalTime;
+		aux++;
+		arrivalTime = 0.0;
+		departureTime = 0.0;
+	}
+
+	double intervalMean(){
+		int i = 0;
+		double j = 0;
+		while(i < aux){
+			j = requisitionInterval[i] + j;
+			i++;
+		}
+		return j/aux;
+	}
+
+	double taskMean(queue<Task> queue){
+		double i = 0;
+		int j = 0;
+		int h = queue.size();
+
+		while(i < h){
+			Task task = queue.back();
+			j = task.sim_time + j;
+			queue.pop();
+			i++;
+		}
+		return j/h;
+	}
+
+	double rand_exp(double lambda){
+		double y = - 1 + ((double) this->crand.GeraNumeroAleatorio() / (RAND_MAX)) + 1;
+		double x;
+
+		return x = -lambda*log(y);
 	}
 };
 
@@ -61,40 +99,41 @@ int main() {
 	double mean_processing = 1.0;
 	double next_arrival = stats.rand_exp(mean_arrival);
 	queue<Task> queue;
+	std::queue<Task> queueStatistics;
 
-	double last_event_time = 0.0;
-	double waiting_time_queue_total = 0.0;
 
 	while (sim_time < T_MAX) {
-//		stats.update();
-//		cout << "1-sim_time: " << sim_time << endl;
-//		cout << "1-next_departure: " << next_departure << endl;
-//		cout << "1-next_arrival: " << next_arrival << endl;
+		stats.update();
 		if (next_arrival < next_departure) {
 			sim_time = next_arrival;
-			//cout << "	2-sim_time: " << sim_time << endl;
 			if (s == IDLE) {
 				s = BUSY;
 				next_departure = sim_time + stats.rand_exp(mean_processing);
-				//cout << "		3-next_departure: " << next_departure << endl;
 			} else {
 				Task task;
+				task.ID = ID++;
+				task.sim_time = sim_time;
 				queue.push(task);
 				cout << "Entrando: " << queue.size() << endl;
 			}
 			next_arrival = sim_time + stats.rand_exp(mean_arrival);
-
+			stats.setarrivalTime(next_arrival);
 		} else {
 			sim_time = next_departure;
 			if (queue.empty()) {
 				s = IDLE;
 				next_departure = HUGE_VAL;
 			} else {
+				Task task = queue.back();
+				task.sim_time -= sim_time;
+				queueStatistics.push(task);
 				queue.pop();
 				cout << "Saindo..." << endl;
 				next_departure = sim_time + stats.rand_exp(mean_processing);
+				stats.setarrivalTime(next_departure);
 			}
 		}
-		last_event_time = sim_time;
 	}
+	cout << "Intervalo medio de Tasks no sistema: " << stats.taskMean(queueStatistics) << endl;
+	cout << "Intervalo medio de requisicoes: " << stats.intervalMean() << endl;
 }
