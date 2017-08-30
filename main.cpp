c00cb52fc40e53e7d7138459b02ef7e04b8ae91a
Lucas Gomes de Farias <lgluucasgoomes@gmail.com>
2017-08-30 19:32:56 -0300

    versao com as 3 metricas

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 #define HUGE_VAL 1000000000000000000000000000000000000000000000000000000.0
-#define T_MAX 100.0
+#define T_MAX 200.0
 static int ID = 0;
 
 enum Server {
@@ -32,9 +32,14 @@ public:
 
 class Statistics {
 private:
+	double arrivalA = 0.0;
+	double arrivalB = 0.0;
 	double arrivalTime = 0.0;
 	double departureTime = 0.0;
 	double requisitionInterval[100];
+	double firstArrival = 0.0;
+	double lastDeparture = 0.0;
+	double totalBusyTime = 0.0;
 	int aux = 0;
 	MetodoCongruente crand = MetodoCongruente(1103515245,12345,2147483648,rand());
 
@@ -43,8 +48,24 @@ public:
 
 	}
 
+	void setfirstArrival(double value){
+		this->firstArrival = value;
+	}
+
+	void setlastDeparture(double value){
+		this->lastDeparture = value;
+	}
+
 	void setarrivalTime(double value){
 		this->arrivalTime = value;
+	}
+
+	void setarrivalA(double value){
+		this->arrivalA = value;
+	}
+
+	void setarrivalB(double value){
+			this->arrivalB = value;
 	}
 
 	void setdepartureTime(double value){
@@ -52,12 +73,13 @@ public:
 	}
 
 	void update() {
-		requisitionInterval[aux] = departureTime - arrivalTime;
+		requisitionInterval[aux] = arrivalB - arrivalA;
 		aux++;
-		arrivalTime = 0.0;
+		arrivalA = 0.0;
+		arrivalB = 0.0;
 		departureTime = 0.0;
 	}
-
+	//intervalo entre requisições
 	double intervalMean(){
 		int i = 0;
 		double j = 0;
@@ -67,19 +89,29 @@ public:
 		}
 		return j/aux;
 	}
-
-	double taskMean(queue<Task> queue){
+	//tempo de medio de todas as task no sistema
+	float taskMean(queue<Task> queue){
 		double i = 0;
-		int j = 0;
+		float j = 0;
 		int h = queue.size();
+		cout << "Tasks: " << h << endl;
 
 		while(i < h){
-			Task task = queue.back();
+			Task task = queue.front();
 			j = task.sim_time + j;
+			//cout << "task.sim_time: " << task.sim_time << endl;
+			//cout << "j: " << j << endl;
+			//cout << "h: " << h << endl;
 			queue.pop();
 			i++;
 		}
+		cout << "j: " << j << endl;
+		this->totalBusyTime = j;
 		return j/h;
+	}
+
+	float serviceMeanTime(){
+		return totalBusyTime/(lastDeparture-firstArrival);
 	}
 
 	double rand_exp(double lambda){
@@ -96,19 +128,25 @@ int main() {
 	double sim_time = 0.0;
 	double next_departure = HUGE_VAL;
 	double mean_arrival = 2.0;
-	double mean_processing = 1.0;
+	double mean_processing = 0.775;
 	double next_arrival = stats.rand_exp(mean_arrival);
+	stats.setfirstArrival(next_arrival);
 	queue<Task> queue;
 	std::queue<Task> queueStatistics;
 
-
+	cout << "next_arrival: " << next_arrival << endl;
 	while (sim_time < T_MAX) {
-		stats.update();
+		if(!queue.empty()) stats.update();
 		if (next_arrival < next_departure) {
 			sim_time = next_arrival;
+			stats.setarrivalA(next_arrival);
+			//cout << "Arrival A: " << next_arrival << endl;
 			if (s == IDLE) {
 				s = BUSY;
+				stats.setarrivalTime(next_arrival);
 				next_departure = sim_time + stats.rand_exp(mean_processing);
+				stats.setdepartureTime(next_departure);
+				cout << "BUSY" << endl;
 			} else {
 				Task task;
 				task.ID = ID++;
@@ -117,7 +155,8 @@ int main() {
 				cout << "Entrando: " << queue.size() << endl;
 			}
 			next_arrival = sim_time + stats.rand_exp(mean_arrival);
-			stats.setarrivalTime(next_arrival);
+			stats.setarrivalB(next_arrival);
+			//cout << "Arrival B: " << next_arrival << endl;
 		} else {
 			sim_time = next_departure;
 			if (queue.empty()) {
@@ -125,15 +164,17 @@ int main() {
 				next_departure = HUGE_VAL;
 			} else {
 				Task task = queue.back();
-				task.sim_time -= sim_time;
+				task.sim_time = sim_time - task.sim_time;
 				queueStatistics.push(task);
 				queue.pop();
 				cout << "Saindo..." << endl;
 				next_departure = sim_time + stats.rand_exp(mean_processing);
-				stats.setarrivalTime(next_departure);
 			}
 		}
 	}
-	cout << "Intervalo medio de Tasks no sistema: " << stats.taskMean(queueStatistics) << endl;
-	cout << "Intervalo medio de requisicoes: " << stats.intervalMean() << endl;
+	cout << "next_departure: " << next_departure << endl;
+	stats.setlastDeparture(next_departure);
+	cout << "1 - Intervalo medio de Tasks no sistema: " << stats.taskMean(queueStatistics) << endl;
+	cout << "2 - Intervalo medio de requisicoes: " << stats.intervalMean() << endl;
+	cout << "3 - Tempo médio de serviço: " << stats.serviceMeanTime() << endl;
 }
